7fcfba0ce3c8b6eb2458a97227a8fb86e7fa73a8
helloyako <helloyako@gmail.com>
2022-05-16 17:34:30 +0900

    Finish Chapter 2

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,62 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, world!")
-      .padding()
-  }
+    @State var game = Game()
+    @State var guess: RGB
+    @State var showScore = false
+
+    var target = RGB.random()
+
+    var body: some View {
+      VStack {
+          Color(rgbStruct: game.target)
+          if !showScore {
+              Text("R: ??? G: ??? B: ???")
+                  .padding()
+          } else {
+              Text(game.target.intString())
+                  .padding()
+          }
+          Color(rgbStruct: guess)
+          Text(guess.intString())
+              .padding()
+          ColorSlider(value: $guess.red, trackColor: .red)
+          ColorSlider(value: $guess.green, trackColor: .green)
+          ColorSlider(value: $guess.blue, trackColor: .blue)
+          Button("Hit me") {
+              showScore = true
+              game.check(guess: guess)
+          }
+          .alert(isPresented: $showScore) {
+              Alert(
+                title: Text("Your Score"),
+                message: Text(String(game.scoreRound)),
+                dismissButton: .default(Text("OK")) {
+                    game.startNewRound()
+                    guess = RGB()
+                })
+          }
+      }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+      ContentView(guess: RGB())
   }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var trackColor: Color
+
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .accentColor(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
+    }
 }
