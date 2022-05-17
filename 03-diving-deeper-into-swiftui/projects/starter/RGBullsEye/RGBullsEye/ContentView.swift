678f61467a29751eac0f9f0eabd33fbd43fdaf86
helloyako <helloyako@gmail.com>
2022-05-17 17:49:53 +0900

    Complete Chapter 3

--- 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/ContentView.swift
+++ 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/ContentView.swift
@@ -37,42 +37,56 @@ struct ContentView: View {
   @State var guess: RGB
   @State var showScore = false
 
-  var body: some View {
-    VStack {
-      ColorCircle(rgb: game.target, size: 200)
-      if !showScore {
-        Text("R: ??? G: ??? B: ???")
-          .padding()
-      } else {
-        Text(game.target.intString)
-          .padding()
-      }
-      ColorCircle(rgb: guess, size: 200)
-      Text(guess.intString)
-        .padding()
-      ColorSlider(value: $guess.red, trackColor: .red)
-      ColorSlider(value: $guess.green, trackColor: .green)
-      ColorSlider(value: $guess.blue, trackColor: .blue)
-      Button("Hit Me!") {
-        self.showScore = true
-        self.game.check(guess: guess)
-      }
-      .alert(isPresented: $showScore) {
-        Alert(
-          title: Text("Your Score"),
-          message: Text(String(game.scoreRound)),
-          dismissButton: .default(Text("OK")) {
-            self.game.startNewRound()
-            self.guess = RGB()
-          })
-      }
+    let circleSize: CGFloat = 0.5
+    let labelWidth: CGFloat = 0.53
+    let labelHeight: CGFloat = 0.06
+    let buttonWidth: CGFloat = 0.87
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+              Color.element
+                  .edgesIgnoringSafeArea(.all)
+
+              VStack {
+                  ColorCircle(rgb: game.target, size: geometry.size.width * circleSize)
+                  if !showScore {
+                    BevelText(
+                      text: "R: ??? G: ??? B: ???", width: geometry.size.width * labelWidth, height: geometry.size.height * labelHeight)
+                  } else {
+                    BevelText(
+                      text: game.target.intString, width: geometry.size.width * labelWidth, height: geometry.size.height * labelHeight)
+                  }
+                  ColorCircle(rgb: guess, size: geometry.size.width * circleSize)
+                  BevelText(text: guess.intString, width: geometry.size.width * labelWidth, height: geometry.size.width * labelHeight)
+                  ColorSlider(value: $guess.red, trackColor: .red)
+                  ColorSlider(value: $guess.green, trackColor: .green)
+                  ColorSlider(value: $guess.blue, trackColor: .blue)
+                  Button("Hit Me!") {
+                    self.showScore = true
+                    self.game.check(guess: guess)
+                  }
+                  .buttonStyle(NeuButtonStyle(width: geometry.size.width * buttonWidth, height: geometry.size.height * labelHeight))
+
+                  .alert(isPresented: $showScore) {
+                    Alert(
+                      title: Text("Your Score"),
+                      message: Text(String(game.scoreRound)),
+                      dismissButton: .default(Text("OK")) {
+                        self.game.startNewRound()
+                        self.guess = RGB()
+                      })
+                  }
+              }
+              .font(.headline)
+            }
+        }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView(guess: RGB())
+      ContentView(guess: RGB())
   }
 }
 
@@ -87,5 +101,6 @@ struct ColorSlider: View {
       Text("255")
     }
     .padding(.horizontal)
+    .font(.subheadline)
   }
 }
--- 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/Model/ViewExtension.swift
+++ 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/Model/ViewExtension.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2022 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -32,30 +32,16 @@
 
 import SwiftUI
 
-struct ColorCircle: View {
-  let rgb: RGB
-  let size: CGFloat
+extension View {
+    func northWestShadow(radius: CGFloat = 16, offset: CGFloat = 6) -> some View {
+        return self
+            .shadow(color: .highlight, radius: radius, x: -offset, y: -offset)
+            .shadow(color: .shadow, radius: radius, x: offset, y: offset)
+    }
 
-  var body: some View {
-      ZStack {
-          Circle()
-              .fill(Color.element)
-              .northWestShadow()
-          Circle()
-              .fill(Color(red: rgb.red, green: rgb.green, blue: rgb.blue))
-              .padding(20)
-      }
-      .frame(width: size, height: size)
-  }
-}
-
-struct ColorCircle_Previews: PreviewProvider {
-  static var previews: some View {
-      ZStack {
-          Color.element
-          ColorCircle(rgb: RGB(), size: 200)
-      }
-      .frame(width: 300, height: 300)
-      .previewLayout(.sizeThatFits)
-  }
+    func southEastShadow(radius: CGFloat = 16, offset: CGFloat = 6) -> some View {
+        return self
+            .shadow(color: .shadow, radius: radius, x: -offset, y: -offset)
+            .shadow(color: .highlight, radius: radius, x: offset, y: offset)
+    }
 }
