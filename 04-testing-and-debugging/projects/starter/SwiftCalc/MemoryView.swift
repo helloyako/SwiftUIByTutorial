52d7706d8e62c397e11fed520de8302a2272cc8c
helloyako <helloyako@gmail.com>
2022-05-17 22:30:13 +0900

    Complete Chapter 4 project

--- 04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
+++ 04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
@@ -29,25 +29,33 @@
 import SwiftUI
 
 struct MemoryView: View {
-  @Binding var memory: Double
-  var geometry: GeometryProxy
+    @Binding var memory: Double
+    var geometry: GeometryProxy
+
 
   var body: some View {
-    HStack {
-      Spacer()
-      Text("\(memory)")
-        .padding(.horizontal, 5)
-        .frame(
-          width: geometry.size.width * 0.85,
-          alignment: .trailing
-        )
-        .overlay(
-          RoundedRectangle(cornerRadius: 8)
-            .stroke(lineWidth: 2)
-            .foregroundColor(Color.gray)
-        )
-      // Add gesture here
-      Text("M")
-    }.padding(.bottom).padding(.horizontal, 5)
+      let memorySwipe = DragGesture(minimumDistance: 20)
+          .onEnded { _ in
+              memory = 0.0
+          }
+      HStack {
+          Spacer()
+          Text("\(memory)")
+              .accessibility(identifier: "memoryDisplay")
+              .padding(.horizontal, 5)
+              .frame(
+                width: geometry.size.width * 0.85,
+                alignment: .trailing
+              )
+              .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(Color.gray)
+              )
+              .gesture(memorySwipe)
+          Text("M")
+      }
+      .padding(.bottom)
+      .padding(.horizontal, 5)
   }
 }
