02428489f85b0cbb4b8517c91931a59f3ab2038c
helloyako <helloyako@gmail.com>
2022-06-29 10:06:36 +0900

    Complete Chapter 17 project

--- 17-drawing-and-custom-graphics/projects/starter/MountainAirport/SearchFlights/FlightTimeHistory.swift
+++ 17-drawing-and-custom-graphics/projects/starter/MountainAirport/SearchFlights/FlightTimeHistory.swift
@@ -33,7 +33,45 @@
 import SwiftUI
 
 struct FlightTimeHistory: View {
-  var flight: FlightInformation
+    var flight: FlightInformation
+    let minuteRange = CGFloat(75)
+
+    func minuteLength(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let pointsPerMinute = proxy.size.width / minuteRange
+        return CGFloat(abs(minutes)) * pointsPerMinute
+    }
+
+    func minuteOffset(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let pointsPerMinute = proxy.size.width / minuteRange
+        let offset = minutes < 0 ? 15 + minutes : 15
+        return CGFloat(offset) * pointsPerMinute
+    }
+
+    func chartGradient(_ history: FlightHistory) -> Gradient {
+        if history.status == .canceled {
+            return Gradient(
+                colors: [
+                    Color.green,
+                    Color.yellow,
+                    Color.red,
+                    Color(red: 0.5, green: 0, blue: 0)
+                ])
+        }
+        if history.timeDifference <= 0 {
+            return Gradient(colors: [Color.green])
+        }
+        if history.timeDifference <= 15 {
+            return Gradient(colors: [Color.green, Color.yellow])
+        }
+        return Gradient(colors: [Color.green, Color.yellow, Color.red])
+    }
+
+    func minuteLocation(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let minMinutes = -15
+        let pointsPerMinute = proxy.size.width / minuteRange
+        let offset = CGFloat(minutes - minMinutes) * pointsPerMinute
+        return offset
+    }
 
   var timeFormatter: RelativeDateTimeFormatter {
     let rtf = RelativeDateTimeFormatter()
@@ -56,17 +94,35 @@ struct FlightTimeHistory: View {
           .font(.title2)
           .padding(.top, 30)
         ScrollView {
-          ForEach(flight.history, id: \.day) { history in
-            HStack {
-              Text("\(history.day) day(s) ago - \(history.flightDelayDescription)")
+            ForEach(flight.history, id: \.day) { history in
+                HStack {
+                    Text("\(history.day) day(s) ago")
+                        .frame(width: 110, alignment: .trailing)
+                  // 1
+                    GeometryReader { proxy in
+                        Rectangle()
+                            .fill(LinearGradient(gradient: chartGradient(history),
+                              startPoint: .leading,
+                              endPoint: .trailing))
+                            .frame(width: minuteLength(history.timeDifference, proxy: proxy))
+                            .offset(x: minuteOffset(history.timeDifference, proxy: proxy))
+
+                        ForEach(-1..<6) { val in
+                            Rectangle()
+                                .stroke(val == 0 ? Color.white : Color.gray, lineWidth: 1.0)
+                                .frame(width: 1)
+                                .offset(x: minuteLocation(val * 10, proxy: proxy))
+                        }
+                    }
+                }
                 .padding()
-              Spacer()
-            }
-            .background(
-              Color.white.opacity(0.2)
-            )
+                .background(Color.white.opacity(0.2))
           }
         }
+          HistoryPieChart(flightHistory: flight.history)
+              .font(.footnote)
+              .frame(width: 250, height: 250)
+              .padding(5)
       }
     }.foregroundColor(.white)
   }
